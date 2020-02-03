2dad6f12f0d7c66753d6cfc8856e528a8573ec1b
Leticia <gl.leticia@gmail.com>
2020-02-03 22:13:14 +0100

    correcting heroku sql

--- lab/tests.py
+++ lab/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
 
 # Create your tests here.
+"""testing module for the tickets app"""
+
+from django.test import TestCase, Client
+from users.models import User
+from .models import Team
+from priority.models import Clients
+
+
+class TeamTest(TestCase):
+    """Testing class for Teams"""
+
+    def setUp(self):
+        """This runs at start of test, creates a user and a client"""
+        self.client = Client()
+        user = User.objects.create(username='Maartin', password='vandeBurg')
+        self.ticket = Clients.objects.create(user=user, department='bug', project='big bug', supervisor='bug')
+
+#    def test_ticket_model_create(self):
+#        """Testing function for ticket creation form"""
+#        user_name = User.objects.create(username='Fred2', password='freddie')
+#        ticket = Ticket.objects.create(author=user_name, title='feature', text='big feature')
+#        assert len(Ticket.objects.all()) == 2
+#
+    def test_view(self):
+        """This checks the tickets view page has loaded correctly"""
+        res = self.client.get('/lab/')
+        assert b'request' in res.content
+
--- users/models.py
+++ users/models.py
@@ -4,5 +4,7 @@ from django.contrib.auth.models import User as AuthUser
 
 class User(AuthUser):
     class Meta:
+        #model= AuthUser
         proxy = True
+
 
